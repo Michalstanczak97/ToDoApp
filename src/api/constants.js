fce0666a15eda68a38ce2db228355e338e8d0bf5
Michal Stanczak <michal.stanczak97@gmail.com>
2021-07-27 15:47:55 +0200

    Todo_finish_reserve_commit

---
+++ src/api/constants.js
@@ -0,0 +1,2 @@
+export const API_URL = "https://todo-api.coderslab.pl/api";
+export const API_KEY = "d48c122c-636e-4bc0-93c5-c2e484172a57";
---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+// import React from 'react';
+// import '../scss/main.scss'
+//
+// const Main = () => {
+//     return (
+//         <div className="main__container">
+//             <div className="main">
+//             <h1>NEW TASK</h1>
+//             <input placeholder="Title"/>
+//             <input placeholder="Description"/>
+//             <button>Add task</button>
+//             </div>
+//         </div>
+//     );
+// };
+//
+// export default Main;
